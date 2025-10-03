dc6e4cf30f284b895cc8dbab14df6cb8e70153b2
Jiadong Lu <56534274+810senpai114514@users.noreply.github.com>
2025-10-03 08:55:26 +0000

    尝试修复错误

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -8,7 +8,7 @@ use crate::{
     mm::{check_user_address, copy_from_user, copy_to_user, translated_refmut, translated_str, VirtAddr, MapPermission},
     syscall::get_syscall_counter,
     task::{
-        add_task, change_program_brk, current_task, current_user_token, exit_current_and_run_next,
+        add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, with_current_memory_set, TaskControlBlock,
     },
     timer::get_time_us,
